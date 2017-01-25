4dcfccff1ded5ea5972d78377e6cd4dfbe963e0f
Antony Ho <ntonyworkshop@gmail.com>
2017-01-25 20:29:28 +0800

    Do not use background download

--- get-mame2003-rom-set.sh
+++ get-mame2003-rom-set.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y unzip
 mkdir -p $HOME/RetroPieInstallation
 cd $HOME/RetroPieInstallation
 echo "Getting MAME2003 0.78 ROM set"
-wget -b https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip
+wget -rc https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip
 
 unzip MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip -d $HOME/RetroPie/roms/mame-libretro

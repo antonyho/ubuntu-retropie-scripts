4cfd068edd67890819379bbd7016d071afac4cba
Antony Ho <ntonyworkshop@gmail.com>
2017-01-26 00:42:26 +0800

    Fix download URL and use cURL

--- get-mame2003-rom-set.sh
+++ get-mame2003-rom-set.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y unzip
 mkdir -p $HOME/RetroPieInstallation
 cd $HOME/RetroPieInstallation
 echo "Getting MAME2003 0.78 ROM set"
-wget -c -O MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip
+wget https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip/roms -C -O MAME2003_Reference_Set_MAME0.78_ROMs.zip
+wget https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip/samples -C -O MAME2003_Reference_Set_MAME0.78_Samples.zip
 
-unzip MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip -d $HOME/RetroPie/roms/mame-libretro
+unzip MAME2003_Reference_Set_MAME0.78_ROMs.zip -d $HOME/RetroPie/roms/mame-libretro
+unzip MAME2003_Reference_Set_MAME0.78_Samples.zip -d $HOME/RetroPie/BIOS/mame2003/samples/

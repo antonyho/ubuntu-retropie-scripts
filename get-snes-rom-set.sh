9a573070ce5a55c34617fe8befdfed0b1fc4257f
Antony Ho <ntonyworkshop@gmail.com>
2017-01-30 00:20:16 +0800

    Fix cookie file problem

--- get-snes-rom-set.sh
+++ get-snes-rom-set.sh
@@ -10,8 +10,12 @@ else
 	mkdir -p $HOME/RetroPieInstallation
 	cd $HOME/RetroPieInstallation
 	echo "Getting SNES no-intro ROM set"
-	curl -c -d "user=$1" -d "password=$2" -d "referer=https://archive.org/" -d "action=login" https://archive.org/account/login.php
-	curl -c https://archive.org/download/No-Intro-Collection_2016-01-03_Fixed/Nintendo%20-%20Super%20Nintendo%20Entertainment%20System.zip -o no-intro-SNES-rom-set.zip
+	curl -c cookie.txt -d "user=$1" -d "password=$2" -d "referer=https://archive.org/" -d "action=login" https://archive.org/account/login.php
+	curl -c cookie.txt https://archive.org/download/No-Intro-Collection_2016-01-03_Fixed/Nintendo%20-%20Super%20Nintendo%20Entertainment%20System.zip -o no-intro-SNES-rom-set.zip
+	
+	if [ -f cookie.txt ]; then
+		rm cookie.txt
+	fi
 
 	unzip $HOME/RetroPieInstallation/no-intro-SNES-rom-set.zip -d $HOME/RetroPie/roms
 fi

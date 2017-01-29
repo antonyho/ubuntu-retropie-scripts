8e28941dc9315f2726a079794223a6bbb20ef697
Antony Ho <ntonyworkshop@gmail.com>
2017-01-30 02:03:42 +0800

    Add archive.org logon

--- get-megadrive-rom-set.sh
+++ get-megadrive-rom-set.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # Get no-intro SEGA Mega Drive - Genesis ROM set
 
-sudo apt-get install -y unzip curl
-mkdir -p $HOME/RetroPieInstallation
-cd $HOME/RetroPieInstallation
-echo "Getting SEGA Mega Drive ROM set"
-curl https://archive.org/download/No-Intro-Collection_2016-01-03_Fixed/Sega%20-%20Mega%20Drive%20-%20Genesis.zip -o no-intro-Sega-MegaDrive-Genesis.zip
-
-unzip $HOME/RetroPieInstallation/no-intro-Sega-MegaDrive-Genesis.zip -d $HOME/RetroPie/roms/megadrive
+if [ $# -lt 2 ]; then
+	echo "This script requires archive.org username and password in order to start downloading."
+	echo "Usage:"
+	echo "./get-megadrive-rom-set.sh <username> <password>"
+else
+	sudo apt-get install -y unzip curl
+	mkdir -p $HOME/RetroPieInstallation
+	cd $HOME/RetroPieInstallation
+	echo "Getting SEGA Mega Drive ROM set"
+	curl https://archive.org/account/login.php -c cookie.txt
+	curl https://archive.org/account/login.php -b cookie.txt -c cookie.txt -F "username=$1" -F "password=$2" -F "referer=https://archive.org/" -F "action=login" -F "submit=Log in"
+	curl https://archive.org/ -b cookie.txt -c cookie.txt
+	curl https://archive.org/download/No-Intro-Collection_2016-01-03_Fixed/Sega%20-%20Mega%20Drive%20-%20Genesis.zip -L -b cookie.txt -c cookie.txt -o $HOME/RetroPieInstallation/no-intro-Sega-MegaDrive-Genesis.zip
+	
+	if [ -f cookie.txt ]; then
+		rm cookie.txt
+	fi
+	
+	unzip $HOME/RetroPieInstallation/no-intro-Sega-MegaDrive-Genesis.zip -d $HOME/RetroPie/roms/megadrive
+fi

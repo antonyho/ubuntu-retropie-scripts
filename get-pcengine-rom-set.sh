a34948b599a52b4584a0be9c52d39232bb1647f5
Antony Ho <ntonyworkshop@gmail.com>
2017-01-31 01:59:49 +0800

    Add get PC Engine no-intro ROM set script

---
+++ get-pcengine-rom-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Get no-intro PC Engine ROM set
+# https://archive.org/details/No-Intro-Collection_2016-01-03_Fixed
+
+if [ $# -lt 2 ]; then
+	echo "This script requires archive.org username and password in order to start downloading."
+	echo "Usage:"
+	echo "./get-pcengine-rom-set.sh <username> <password>"
+else
+	sudo apt-get install -y unzip curl
+	mkdir -p $HOME/RetroPieInstallation
+	cd $HOME/RetroPieInstallation
+	echo "Getting PC Engine no-intro ROM set"
+	curl https://archive.org/account/login.php -c cookie.txt
+	curl https://archive.org/account/login.php -b cookie.txt -c cookie.txt -F "username=$1" -F "password=$2" -F "referer=https://archive.org/" -F "action=login" -F "submit=Log in"
+	curl https://archive.org/ -b cookie.txt -c cookie.txt
+	curl https://archive.org/download/No-Intro-Collection_2016-01-03_Fixed/NEC%20-%20PC%20Engine%20-%20TurboGrafx%2016.zip -L -b cookie.txt -c cookie.txt -o $HOME/RetroPieInstallation/no-intro-pcengine-rom-set.zip
+	
+	if [ -f cookie.txt ]; then
+		rm cookie.txt
+	fi
+
+	unzip $HOME/RetroPieInstallation/no-intro-pcengine-rom-set.zip -d $HOME/RetroPie/roms/pcengine/
+fi

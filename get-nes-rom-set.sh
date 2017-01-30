fdc2449f4bb0b4ef4c00910dafa63dd6adb353a1
Antony Ho <ntonyworkshop@gmail.com>
2017-01-30 18:23:20 +0800

    Add get NES no-intro ROM set script

---
+++ get-nes-rom-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Get no-intro NES(Nintendo Entertainment System) ROM set
+# https://archive.org/details/No-Intro-Collection_2016-01-03_Fixed
+
+if [ $# -lt 2 ]; then
+	echo "This script requires archive.org username and password in order to start downloading."
+	echo "Usage:"
+	echo "./get-nes-rom-set.sh <username> <password>"
+else
+	sudo apt-get install -y unzip curl
+	mkdir -p $HOME/RetroPieInstallation
+	cd $HOME/RetroPieInstallation
+	echo "Getting NES no-intro ROM set"
+	curl https://archive.org/account/login.php -c cookie.txt
+	curl https://archive.org/account/login.php -b cookie.txt -c cookie.txt -F "username=$1" -F "password=$2" -F "referer=https://archive.org/" -F "action=login" -F "submit=Log in"
+	curl https://archive.org/ -b cookie.txt -c cookie.txt
+	curl https://archive.org/download/No-Intro-Collection_2016-01-03_Fixed/Nintendo%20-%20Nintendo%20Entertainment%20System.zip -L -b cookie.txt -c cookie.txt -o $HOME/RetroPieInstallation/no-intro-NES-rom-set.zip
+	
+	if [ -f cookie.txt ]; then
+		rm cookie.txt
+	fi
+
+	unzip $HOME/RetroPieInstallation/no-intro-NES-rom-set.zip -d $HOME/RetroPie/roms/nes/
+fi

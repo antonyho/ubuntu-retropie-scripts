c893f963147f61249812e289e9885a24e848ed57
Antony Ho <ntonyworkshop@gmail.com>
2017-01-31 18:54:55 +0800

    Add description to the script

--- add-game-launching-splash.sh
+++ add-game-launching-splash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Show a default splash screen when no game art is available in gamelist.xml
+# https://retropie.org.uk/forum/topic/4611/runcommand-system-splashscreens/2
 
 command -v unzip >/dev/null 2>&1 || { sudo apt-get install -y unzip; }
 

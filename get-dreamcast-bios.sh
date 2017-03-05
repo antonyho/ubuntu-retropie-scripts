a3ef06536f414cc52c9f7617c5db4a91c17286e5
Antony Ho <ntonyworkshop@gmail.com>
2017-03-05 13:59:42 +0800

    Use Japanese BIOS

--- get-dreamcast-bios.sh
+++ get-dreamcast-bios.sh
@@ -11,7 +11,7 @@
 command -v unzip >/dev/null 2>&1 || { sudo apt-get install -y unzip; }
 
 mkdir -p $HOME/RetroPieInstallation/dc/BIOS
-curl https://www.loveroms.com/extras/files/bios/dreamcast-bios.zip -o $HOME/RetroPieInstallation/dc/dreamcast-bios.zip
+curl http://50.7.136.26/as981lLNbabc6u10hHaksHcb122/epforums/upload/2/3/4/5/2/821811740799914687.zip -o $HOME/RetroPieInstallation/dc/dreamcast-bios.zip
 unzip $HOME/RetroPieInstallation/dc/dreamcast-bios.zip -d $HOME/RetroPieInstallation/dc/BIOS/
-cp "$HOME/RetroPieInstallation/dc/BIOS/DC - BIOS (All).bin" $HOME/RetroPie/BIOS/dc_boot.bin
-cp "$HOME/RetroPieInstallation/dc/BIOS/DC - Flash (All).bin" $HOME/RetroPie/BIOS/dc_flash.bin
+cp "$HOME/RetroPieInstallation/dc/BIOS/SEGA_Dreamcast_BIOS/dc_bios.bin" $HOME/RetroPie/BIOS/
+cp "$HOME/RetroPieInstallation/dc/BIOS/SEGA_Dreamcast_Flash_BIOS_J/dc_flash.bin" $HOME/RetroPie/BIOS/dc_flash.bin

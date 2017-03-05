8e9aed69e174d046f444b19b3f5e83dc522efcdb
Antony Ho <ntonyworkshop@gmail.com>
2017-03-05 13:42:17 +0800

    Update download filename

--- get-dreamcast-bios.sh
+++ get-dreamcast-bios.sh
@@ -11,7 +11,7 @@
 command -v unzip >/dev/null 2>&1 || { sudo apt-get install -y unzip; }
 
 mkdir -p $HOME/RetroPieInstallation/dc/BIOS
-curl https://www.loveroms.com/extras/files/bios/dreamcast-bios.zip -o $HOME/RetroPieInstallation/dc/
+curl https://www.loveroms.com/extras/files/bios/dreamcast-bios.zip -o $HOME/RetroPieInstallation/dc/dreamcast-bios.zip
 unzip $HOME/RetroPieInstallation/dc/dreamcast-bios.zip -d $HOME/RetroPieInstallation/dc/BIOS/
 cp "$HOME/RetroPieInstallation/dc/BIOS/DC - BIOS (All).bin" $HOME/RetroPie/BIOS/dc_boot.bin
 cp "$HOME/RetroPieInstallation/dc/BIOS/DC - Flash (All).bin" $HOME/RetroPie/BIOS/dc_flash.bin
